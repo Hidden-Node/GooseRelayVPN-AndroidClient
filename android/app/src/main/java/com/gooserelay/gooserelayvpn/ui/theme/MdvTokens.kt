ae67faa3098b2d01cbeeb174b0d7330eedbd0791
Hidden-Node <mr.regista6@gmail.com>
2026-09-23 11:39:02 +0330

    chore(android): delete unused MdvElevation, MdvMotion, MdvType tokens and dead color vals

--- android/app/src/main/java/com/gooserelay/gooserelayvpn/ui/theme/MdvTokens.kt
+++ android/app/src/main/java/com/gooserelay/gooserelayvpn/ui/theme/MdvTokens.kt
@@ -2,7 +2,6 @@ package com.gooserelay.gooserelayvpn.ui.theme
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 object MdvColor {
     val Background = Color(0xFF101814)
@@ -11,17 +10,14 @@ object MdvColor {
     val Surface = Color(0xFF1C2620)
     val SurfaceHigh = Color(0xFF26312A)
     val SurfaceHighest = Color(0xFF313C35)
-    val SurfaceBright = Color(0xFF35403A)
 
     val Primary = Color(0xFFC4F9D5)
     val PrimaryContainer = Color(0xFF19D16A)
     val PrimaryDim = Color(0xFF10B85A)
     val Secondary = Color(0xFFBED7C6)
-    val Tertiary = Color(0xFFE4FFEA)
 
     val OnSurface = Color(0xFFDFEBE3)
     val OnSurfaceVariant = Color(0xFFBDD0C2)
-    val Outline = Color(0xFF8BA194)
     val OutlineVariant = Color(0xFF3C4D42)
 
     val Error = Color(0xFFFFB4AB)
@@ -44,31 +40,4 @@ object MdvRadius {
     val Md = 12.dp
     val Lg = 16.dp
     val Xl = 20.dp
-}
-
-object MdvElevation {
-    val E0 = 0.dp
-    val E1 = 2.dp
-    val E2 = 6.dp
-    val E3 = 12.dp
-    val Focus = 18.dp
-}
-
-object MdvMotion {
-    const val FastMs = 160
-    const val NormalMs = 280
-    const val SlowMs = 420
-    const val PulseMs = 820
-}
-
-object MdvType {
-    val DisplayLg = 34.sp
-    val HeadlineMd = 28.sp
-    val TitleLg = 22.sp
-    val TitleMd = 18.sp
-    val BodyLg = 16.sp
-    val BodyMd = 14.sp
-    val BodySm = 12.sp
-    val LabelMd = 11.sp
-    val LabelSm = 10.sp
 }
